fde90ffdd7c42825115fd6f03e31a1c203f551cb
Sebastian Saldarriaga <sebastian.saldarriaga.ramirez@gmail.com>
2021-01-16 15:19:21 -0500

    nstalled and config styled components and react router

--- rollup.config.common.js
+++ rollup.config.common.js
@@ -3,15 +3,17 @@ import clear from 'rollup-plugin-clear';
 import commonjs from '@rollup/plugin-commonjs';
 import typescript from '@rollup/plugin-typescript';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
+import createStyledComponentsTransformer from 'typescript-plugin-styled-components';
 
 const OUTPUT_DIR = './public/js/';
+
+const styledComponentsTransformer = createStyledComponentsTransformer();
 
 const PLUGINS = [
   clear({
     targets: [`${OUTPUT_DIR}esm`, `${OUTPUT_DIR}system`],
     watch: true,
   }),
-  typescript(),
   nodeResolve({
     jsnext: true,
     browser: true,
@@ -19,26 +21,13 @@ const PLUGINS = [
   }),
   commonjs({
     include: 'node_modules/**',
-    namedExports: {
-      './node_modules/react/index.js': [
-        'cloneElement',
-        'createElement',
-        'PropTypes',
-        'Children',
-        'Component',
-        'createFactory',
-        'PureComponent',
-        'lazy',
-        'Suspense',
-        'useState',
-        'useEffect',
-      ],
-      './node_modules/react-dom/index.js': ['findDOMNode'],
-      './node_modules/babel-runtime/node_modules/core-js/library/modules/es6.object.to-string.js': ['default'],
-      './node_modules/process/browser.js': ['nextTick'],
-      './node_modules/events/events.js': ['EventEmitter'],
-      './node_modules/react-is/index.js': ['isValidElementType'],
-    },
+  }),
+  typescript({
+    transformers: [
+      () => ({
+        before: [styledComponentsTransformer],
+      }),
+    ],
   }),
   babel({
     exclude: 'node_modules/**',
